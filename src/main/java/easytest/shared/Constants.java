945667ad2a0d386e76d6ce54ee6b1c9b5c1bf1b6
Afnan Ahmad <34241491+afnan-ahmad@users.noreply.github.com>
2024-11-28 18:10:10 +0530

    Initial commit

---
+++ src/main/java/easytest/shared/Constants.java
@@ -0,0 +1,13 @@
+package easytest.shared;
+
+public class Constants {
+
+  public static final String ENABLED = "enabled";
+  public static final String DISABLED = "disabled";
+
+  public static final String PLUGIN = "plugin";
+  public static final String PIPELINE = "pipeline";
+  public static final String NAMESPACE = "namespace";
+  public static final String LOGICAL_START_TIME = "logical_start_time";
+
+}
