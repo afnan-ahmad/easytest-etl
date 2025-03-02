e19162aeb24b48482ef1818a9e45fdf4ce542d5f
Afnan Ahmad <34241491+afnan-ahmad@users.noreply.github.com>
2025-03-02 10:32:43 +0530

    Mutation plugin
    Initial commit for mutation plugin

--- src/main/java/easytest/shared/Constants.java
+++ src/main/java/easytest/shared/Constants.java
@@ -10,4 +10,12 @@ public class Constants {
   public static final String NAMESPACE = "namespace";
   public static final String LOGICAL_START_TIME = "logical_start_time";
 
+  public static final String MUTATION_NULL = "Set to null";
+  public static final String MUTATION_ZERO = "Set to zero";
+  public static final String MUTATION_NEGATIVE = "Set to negative";
+  public static final String MUTATION_ABSOLUTE = "Set to absolute";
+  public static final String MUTATION_SPECIFIC = "Set to specified value";
+  public static final String MUTATION_LARGE_NEGATIVE = "Set to large negative value";
+  public static final String MUTATION_LARGE_POSITIVE = "Set to large positive value";
+
 }
